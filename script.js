7cc1be2540aaa0e0e70621b2281cfcbe2bc1e3b3
nicolas.limacher <nicolas.limacher@sovos.com>
2019-12-13 12:49:32 -0500

    added random album generation, changed layout in poll form

--- script.js
+++ script.js
@@ -11,21 +11,21 @@ function savePollToFirebase(best, worst, middle, middle2) {
       });
 
 }
-
 function populatePoll() {
-    var ref = firebase.database().ref('Albums');
-    var albumList = new Array();
-    ref.on("value", function(snapshot) {
-      snapshot.forEach(function(childSnapshot){
-        console.log(childSnapshot.val());
-        albumList.push(childSnapshot.val());
-      });
-    });
+  var ref = firebase.database().ref('Albums');
+  ref.on('value', function(snap) {
+    var albums = snap.val();
+    changeAlbumNames(albums);
+  });
+}
+function changeAlbumNames(albums) {
 
-    console.log(albumList)
+  //select 4 random elements from albums
+  const shuffled = albums.sort( () => 0.5 - Math.random() ); // Shuffle array
+  let selected = shuffled.slice(0, 4); // Get sub-array of first four elements after shuffled
 
-    document.getElementById("album-1-name").innerHTML = "hello";
-    document.getElementById("album-2-name").innerHTML = albumList[1];
-    document.getElementById("album-3-name").innerHTML = albumList[2];
-    document.getElementById("album-4-name").innerHTML = albumList[3];
+  document.getElementById("album-1-name").innerHTML = selected[0];
+  document.getElementById("album-2-name").innerHTML = selected[1];
+  document.getElementById("album-3-name").innerHTML = selected[2];
+  document.getElementById("album-4-name").innerHTML = selected[3];
 }
